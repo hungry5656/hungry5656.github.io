8e385ec00eda2953957babe33d8fa9817aa1c221
hungry5656 <hhhhhenrysun@gmail.com>
2023-07-23 22:49:00 -0700

    adding components

---
+++ src/components/About/about.tsx
@@ -0,0 +1,12 @@
+import styles from '../../css/page.module.css'
+
+
+export default function about() {
+    return (
+        <main className={styles.main}>
+            <div>
+                This is about page.
+            </div>
+        </main>
+    )
+}
