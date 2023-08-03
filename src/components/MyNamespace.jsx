e1806010047b5da32f0861b38f792c42fbd88ab0
hungry5656 <hhhhhenrysun@gmail.com>
2023-08-03 02:15:26 -0700

    finish logo

--- src/components/MyNamespace.jsx
+++ src/components/MyNamespace.jsx
@@ -2,12 +2,18 @@
 
 import Link from 'next/link';
 import {NAMESPACE_CONST} from "../constants"
+// import {} from ""
+// import '../scss/header.module.scss'
 
 export function MyNamespace(){
     return (
         <ul className="navbar-nav navbar-left justify-content-left w-25">
                 <li key={NAMESPACE_CONST.id}>
-                    <Link href="/" aria-label="Go to home page" role="link">
+                    <Link href="/">
+                        <span class="rounded-circle d-inline-block border-2 border-black">
+                            <img src={NAMESPACE_CONST.url} width="30" height="30" className="rounded-circle d-inline-block align-center" alt="Logo" />
+                        </span>
+                        &nbsp;
                         {NAMESPACE_CONST.name}
                     </Link>
                 </li>
--- src/constants/namespace.jsx
+++ src/constants/namespace.jsx
@@ -1,5 +1,5 @@
 export const NAMESPACE_CONST = {
     id: "0",
     name: "hungry5656",
-    url: "/public/favicons/android-chrome-192x192.png",
+    url: "/favicons/android-chrome-192x192.png",
 }
