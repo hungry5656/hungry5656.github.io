ad324863b9d39d511a52d1bbf4d44af0082ae94e
hungry5656 <hhhhhenrysun@gmail.com>
2023-08-14 00:06:50 -0700

    save current updates

--- src/components/MyHeroPage.jsx
+++ src/components/MyHeroPage.jsx
@@ -1,10 +1,21 @@
+import { useEffect, useState, useRef } from "react";
 
 
 export function MyHeroPage(){
     return (
-        <div>
+        <div className="">
+            <h1>
+                Hi there 
+                <div className="d-inline-block">
+                    <img src="https://media.giphy.com/media/hvRJCLFzcasrR4ia7z/giphy.gif" width="25px" />
+                </div>
+                , I&apos;m Sizhuo (Henry).
+            </h1>
+            <h2>
+                I am 
+            </h2>
             <p>
-                Hi Everyone, my name is <>Sizhuo Sun</>. You can call me <>Henry</> if you want. <br />
+                My name is <>Sizhuo Sun</>. You can call me <>Henry</> if you want. <br />
                 I am currently a senior studying Computer Science and Engineering at UC Davis. 
             </p>
         </div>
---
+++ src/constants/info.jsx
@@ -0,0 +1,7 @@
+import { useEffect, useState, useRef } from "react";
+export const MY_DISCRIPTION = [
+    "a rising senior CSE student",
+    "a #Future Engineer",
+    "an aggie from UC Davis",
+    "coding to make the world better"
+];
